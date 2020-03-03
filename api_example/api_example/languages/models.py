e2e2a9b38105fcf8c2523722a98ee61f4d080564
muhammedhassanm <31989237+muhammedhassanm@users.noreply.github.com>
2020-03-03 11:54:21 +0530

    1

--- api_example/api_example/languages/models.py
+++ api_example/api_example/languages/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Language(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50,)
     paradigm = models.CharField(max_length=50)
 
     def __str__(self):
